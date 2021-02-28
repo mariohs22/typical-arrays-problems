fece3a886eb401582ea140d6b93cf176e0dc27b4
Siarhei Burakouski <serge.burakovsky@gmail.com>
2021-02-28 16:12:19 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 
 exports.min = function min (array) {
-  if (!array) return 0;
-  return Math.min.apply(null, array);
+  if (array === undefined || array.length == 0) return 0;
+  return Math.min.apply(null, array) || 0;
 }
 
 exports.max = function max (array) {
-  if (!array) return 0;
-  return Math.max.apply(null, array);
+  if (array === undefined || array.length == 0) return 0;
+  return Math.max.apply(null, array) || 0;
 }
 
 exports.avg = function avg (array) {
-  if (!array) return 0;
-  var total = 0;
-  for(var i = 0; i < array.length; i++) {
-      total += array[i];
-  }
-  var avg = total / array.length;
-  return 0;
+  if (array === undefined || array.length == 0) return 0;
+  return array.reduce((a, b) => a + b) / array.length;
 }
