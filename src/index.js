8acebc7f508b1e6bc57521358368a68e615f77f4
Mariohs22 <serge.burakovsky@gmail.com>
2021-02-28 08:33:38 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) return 0;
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0;
+  return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
+  if (!array) return 0;
+  var total = 0;
+  for(var i = 0; i < array.length; i++) {
+      total += array[i];
+  }
+  var avg = total / array.length;
   return 0;
 }
